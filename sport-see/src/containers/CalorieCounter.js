435473a7a8810f590cfbec2670331b9702edfb77
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-01-31 16:19:01 +0100

    test : ajout des données api dans le block compteur de calories

--- sport-see/src/containers/CalorieCounter.js
+++ sport-see/src/containers/CalorieCounter.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import Counter_block from '../components/Counter_block';
+import React from "react";
+import { useFetch } from "../api/useFetch";
+import Counter_block from "../components/Counter_block";
 
-import '../styles/caloriesCounter.css'
+import "../styles/caloriesCounter.css";
 
 const CalorieCounter = () => {
-    return (
-        <div className='caloriesCounter_container'>
-            <Counter_block />
-        </div>
-    );
+ const { data, isLoading, error } = useFetch("http://localhost:4000/user/12");
+ const keyData = data.data.keyData;
+ console.log(keyData)
+  return (
+    <div className="caloriesCounter_container">
+      <Counter_block
+        calorieValue = {keyData.calorieCount}
+        proteinValue = {keyData.proteinCount}
+        carbohydrateValue = {keyData.carbohydrateCount}
+        lipidValue = {keyData.lipidCount}
+      />
+    </div>
+  );
 };
 
 export default CalorieCounter;
