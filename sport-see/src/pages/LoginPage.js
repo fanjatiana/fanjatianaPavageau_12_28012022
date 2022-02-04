b0f44f4fd738b419ea7eccc7fe2a50a03a0b84bb
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-02-04 10:43:07 +0100

    ajout des avatars dans la page login

--- sport-see/src/pages/LoginPage.js
+++ sport-see/src/pages/LoginPage.js
@@ -2,17 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { USER_MAIN_DATA } from "../api/data.js";
 import "../styles/loginPage.css";
-
+import AvatarMen from "../assets/men.jpg";
+import AvatarWomen from "../assets/women.jpg";
 const LoginPage = () => {
   return (
     <main className="login_container">
-      <h2>Connexion</h2>
+      <h2>Bienvenue sur SportSee</h2>
       <p>Veuillez selectionner un utilisateur</p>
       <ul className="user_list">
         {USER_MAIN_DATA.map((user) => (
           <li>
             <NavLink to={`/user/${user.id}`}>
               {user.userInfos.firstName}
+              {user.id=== 12? <img className="avatar" src={AvatarMen}/> : <img className="avatar" src={AvatarWomen}/> }
             </NavLink>
           </li>
         ))}
